644c7848b4135ebd20fc7e7f73aa34bc2ab69aee
Narain Sagar <narainmenghwar@gmail.com>
2019-10-13 04:09:51 +0500

    add verbose

--- copy-assets.sh
+++ copy-assets.sh
@@ -5,6 +5,6 @@ set -x
 # copy .env file to dist/
 #cp .env dist
 # Copy all the view templates to dist/
-cp -a views dist
+cp -av views dist
 # Copy all the assets to dist/
-cp -a public dist
+cp -av public dist
