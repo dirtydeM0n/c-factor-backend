47b64a9732d4712c95821951be46258cc6e083ef
Narain Sagar <narainmenghwar@gmail.com>
2019-11-18 03:29:53 +0100

    added user gameplays API endpoints & game status + few more

--- src/routes/user/user.router.ts
+++ src/routes/user/user.router.ts
@@ -5,6 +5,9 @@ import UserController from './user.controller';
 const UserRouter = Router()
   .get('/campaigns', UserController.getAllUsersCampaigns)
   .get('/competencies', UserController.getAllUsersCompetencies)
+  .get('/gameplay', UserController.getAllUsersGamePlayCount)
+  .get('/gameplay/count', UserController.getAllUsersGamePlayCount)
+  .get('/count', UserController.getUsersCount)
   .get('/', UserController.getAll)
   .post('/', UserController.post)
   .get('/:id', UserController.getById)
@@ -27,22 +30,34 @@ const UserRouter = Router()
   .put('/:id/campaigns/:campaignId/competencies/:competencyId', UserController.editCompetency)
   .post('/:id/campaigns/:campaignId/competencies/:competencyId', UserController.editCompetency)
   .delete('/:id/campaigns/:campaignId/competencies/:competencyId', UserController.deleteCompetency)
+
+  // User Game plays
+  .get('/:userId/gameplay', UserController.getUserGamePlayCountById)
+  .get('/:userId/gameplay/count', UserController.getUserGamePlayCountById)
+
   // User Campaigns
   .get('/:userId/campaigns', UserController.getUserCampaigns)
   .get('/:userId/:campaignId', UserController.getUserCampaignById)
   .put('/:userId/:campaignId', UserController.updateUserCampaign)
   .post('/:userId/:campaignId', UserController.updateUserCampaign)
   .delete('/:userId/:campaignId', UserController.deleteUserCampaign)
-  .get('/:userId/:campaignId/score', UserController.getUserCampaignById)
-  .put('/:userId/:campaignId/score', UserController.updateUserCampaign)
-  .post('/:userId/:campaignId/score', UserController.updateUserCampaign)
-  .post('/:userId/:campaignId/status', UserController.updateUserCampaign)
   .post('/:userId/:campaignId/assign', UserController.updateUserCampaign)
+  .get('/:userId/:campaignId/score', UserController.getUserCampaignScore)
+  .put('/:userId/:campaignId/score', UserController.updateUserCampaignScore)
+  .post('/:userId/:campaignId/score', UserController.updateUserCampaignScore)
+  .get('/:userId/:campaignId/status', UserController.getUserCampaignStatus)
+  .put('/:userId/:campaignId/status', UserController.updateUserCampaignStatus)
+  .post('/:userId/:campaignId/status', UserController.updateUserCampaignStatus)
   // Competencies
+  .get('/:userId/:campaignId/:competencyId', UserController.getUserCompetencyById)
   .put('/:userId/:campaignId/:competencyId', UserController.saveUserCompetency)
   .post('/:userId/:campaignId/:competencyId', UserController.saveUserCompetency)
-  .put('/:userId/:campaignId/:competencyId/score', UserController.saveUserCompetency)
-  .post('/:userId/:campaignId/:competencyId/score', UserController.saveUserCompetency);
+  .get('/:userId/:campaignId/:competencyId/score', UserController.getUserCompetencyScore)
+  .put('/:userId/:campaignId/:competencyId/score', UserController.saveUserCompetencyScore)
+  .post('/:userId/:campaignId/:competencyId/score', UserController.saveUserCompetencyScore)
+  .get('/:userId/:campaignId/:competencyId/status', UserController.getUserCompetencyStatus)
+  .put('/:userId/:campaignId/:competencyId/status', UserController.saveUserCompetencyStatus)
+  .post('/:userId/:campaignId/:competencyId/status', UserController.saveUserCompetencyStatus);
 
 export { UserRouter };
 export { AvatarRouter } from './avatar/avatar.router';
